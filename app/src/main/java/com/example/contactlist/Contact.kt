4a048ed033d1f5d1430628229cc8f511cbe58e89
ThiagoDeAM <thiagomaciel822@gmail.com>
2025-03-28 10:16:06 -0300

    Adapter na MainActivity

--- app/src/main/java/com/example/contactlist/Contact.kt
+++ app/src/main/java/com/example/contactlist/Contact.kt
@@ -1,9 +1,13 @@
 package com.example.contactlist
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Contact(
     var id: Int? = -1,
     var name: String = "",
     var address: String = "",
     var phone: String = "",
     var email: String = ""
-)
+): Parcelable
--- app/src/main/java/com/example/contactlist/MainActivity.kt
+++ app/src/main/java/com/example/contactlist/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.contactlist
 
+//noinspection SuspiciousImport
+import android.R
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,8 +14,38 @@ class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    //Data Source
+    private val contactList: MutableList<Contact> = mutableListOf()
+
+    // Adapter
+    private val contactAdapter: ArrayAdapter<String> by lazy {
+        ArrayAdapter(this, R.layout.simple_list_item_1,
+            contactList.map {
+                "${it.name} - ${it.email}"
+            })
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        fillContactList()
+
+        amb.contactLv.adapter = contactAdapter
+    }
+
+    private fun fillContactList(){
+        for (i in 1 .. 50){
+            contactList.add(
+                Contact(
+                    1,
+                    "nome $i",
+                    "endereco $i",
+                    "phone $i",
+                    "email $i"
+                )
+            )
+        }
     }
 }
