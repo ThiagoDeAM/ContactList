96c3c16d4622dfc2ceab6adddfe99905ac089dbb
ThiagoDeAM <thiagomaciel822@gmail.com>
2025-05-09 09:03:26 -0300

    refactor: add query annotations

--- app/src/main/java/com/example/contactlist/model/ContactDao.kt
+++ app/src/main/java/com/example/contactlist/model/ContactDao.kt
@@ -1,9 +1,15 @@
 package com.example.contactlist.model
 
+@Dao
 interface ContactDao {
+    @Insert
     fun createContact(contact: Contact): Long
+    @Query("SELECT * FROM Contact WHERE id = :id")
     fun retrieveContact(id: Int): Contact
+    @Query("SELECT * FROM Contact")
     fun retrieveContacts(): MutableList<Contact>
+    @Update
     fun updateContact(contact: Contact): Int
-    fun deleteContact(id: Int): Int
+    @Delete
+    fun deleteContact(contact: Contact): Int
 }
