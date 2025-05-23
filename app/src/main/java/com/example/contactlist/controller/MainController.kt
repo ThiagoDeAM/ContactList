51d64b9c6ce5af955b7b22eee0c4429d215a3c21
ThiagoDeAM <thiagomaciel822@gmail.com>
2025-05-23 11:23:34 -0300

    refactor: add coroutines

--- app/src/main/java/com/example/contactlist/controller/MainController.kt
+++ app/src/main/java/com/example/contactlist/controller/MainController.kt
@@ -1,51 +1,65 @@
 package com.example.contactlist.controller
 
+import android.os.Message
 import android.widget.Toast
 import androidx.room.Room
+import com.example.contactlist.model.Constant
+import com.example.contactlist.model.Constant.EXTRA_CONTACT_ARRAY
 import com.example.contactlist.model.Contact
 import com.example.contactlist.model.ContactDao
+import com.example.contactlist.model.ContactFirebaseDatabase
 import com.example.contactlist.model.ContactRoomDb
 import com.example.contactlist.model.ContactSqlite
 import com.example.contactlist.ui.MainActivity
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class MainController(mainActivity: MainActivity) {
+class MainController(private val mainActivity: MainActivity) {
     //private val contactDao: ContactDao = ContactSqlite(mainActivity)
 
+    /*
     private val contactDao: ContactDao = Room.databaseBuilder(
         mainActivity,
         ContactRoomDb::class.java,
         "contact-database"
-    ).build().contactDao()
+    ).build().contactDao()*/
+
+    private val contactDao: ContactDao = ContactFirebaseDatabase()
+    private val databaseCoroutineScope = CoroutineScope(Dispatchers.IO)
 
     fun insertContact(contact: Contact) {
-        //substituido thread {}.start() por MainScope (corroutine)
-        MainScope().launch {
-            withContext(Dispatchers.IO) {
-                contactDao.createContact(contact)
-            }
+        //substituido thread {}.start() por MainScope (coroutine)
+        databaseCoroutineScope.launch {
+            contactDao.createContact(contact)
         }
     }
     //E se fosse pra retornar um Long num toast? "O id retornado foi: ..." Como ficaria nesse caso das threads?
-    // EXERCÍCIO: A parte de modificação, remoção, fazer usando corrotinas...
 
-    fun getContact(id: Int) = contactDao.retrieveContact(id)
-    fun getContacts() = contactDao.retrieveContacts()
-    fun modifyContact(contact: Contact){
-        MainScope().launch {
-            withContext(Dispatchers.IO) {
-                contactDao.updateContact(contact)
-            }
+    fun getContact(id: Int) = databaseCoroutineScope.launch {
+        contactDao.retrieveContact(id)
+    }
+
+    fun getContacts() {
+        databaseCoroutineScope.launch {
+            val contactList = contactDao.retrieveContacts()
+            mainActivity.getContactsHandler.sendMessage(Message().apply {
+                data.putParcelableArray(EXTRA_CONTACT_ARRAY, contactList.toTypedArray())
+            })
         }
     }
+
+    fun modifyContact(contact: Contact){
+        databaseCoroutineScope.launch {
+            contactDao.updateContact(contact)
+        }
+    }
+
     fun removeContact(contact: Contact) {
-        MainScope().launch {
-            withContext(Dispatchers.IO) {
-                contactDao.deleteContact(contact)
-            }
+        databaseCoroutineScope.launch {
+            contactDao.deleteContact(contact)
         }
     }
 }
